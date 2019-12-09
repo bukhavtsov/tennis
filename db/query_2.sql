6b08c2d407cb24d0e8f14fac1261566081e04c8a
artyom <artyom.bukhavtsov@gmail.com>
2019-12-09 09:30:26 +0300

    exercise_2 has been done

--- db/query_2.sql
+++ db/query_2.sql
@@ -7,5 +7,5 @@ SELECT first_name, last_name, country.name AS country FROM sportsmen INNER JOIN 
 --Query returns stadium name and country from The Federal Republic of Germany or The Russian Federation
 SELECT * FROM stadium INNER JOIN country ON stadium.country_id = country.id WHERE country.name = 'The Federal Republic of Germany' or country.name = 'The Russian Federation';
 
---Query returns tournament name and prize found
+--Query returns tournament name and prize found that bigger than 100_000 and less 300_000
 SELECT tournament.name, prize_found.prize_money FROM tournament INNER JOIN prize_found on tournament.prize_found_id = prize_found.id where (prize_found.prize_money > cast(100000 as money) and prize_found.prize_money < cast(300000 as money));
